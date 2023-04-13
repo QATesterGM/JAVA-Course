114e98826ff3e1aaf0ee51ee10996319bc3f929d
Lukasz <gajdalukasz89@gmail.com>
2023-04-13 07:21:44 +0200

    Exercise equal cases added

--- src/ExerciseEqualCases.java
+++ src/ExerciseEqualCases.java
@@ -10,8 +10,28 @@ public class ExerciseEqualCases {
 
         String s = str1.toUpperCase();
         System.out.println(s);
-
         String s1 = str1.toLowerCase();
         System.out.println(s1);
+
+        boolean startsWith = str1.startsWith("He");
+        System.out.println(startsWith);
+        System.out.println(str1.endsWith("lo"));
+        System.out.println(str1.contains("ll"));
+
+        System.out.println("    ".isBlank()); //ignoruje spacje
+        System.out.println("  ".isEmpty());
+
+        String replace = str1.replace("llo", "QWE");
+        System.out.println(replace);
+
+        String sra = str1.replaceAll("ll", "GG");
+        System.out.println(sra);
+
+        String textWithWhiteSpaces = "   to jest jakiś text    ";
+        System.out.println(textWithWhiteSpaces.length());
+
+        String strip = textWithWhiteSpaces.strip();
+        System.out.println(strip);
+        System.out.println(strip.length());
     }
 }
