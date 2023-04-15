c65ff7babe25b9c93ea13ee4143605a7a45ce593
Lukasz <gajdalukasz89@gmail.com>
2023-04-15 10:15:33 +0200

    Access modifiers package added

--- src/ModyfikatoryDostepu/ModyfikatoryDostepu.java
+++ src/ModyfikatoryDostepu/ModyfikatoryDostepu.java
@@ -1,8 +1,13 @@
 package ModyfikatoryDostepu;
 
+import ModyfikatoryDostepu.model.Car;
+
 public class ModyfikatoryDostepu {
 
     public static void main(String[] args){
+
+        Car pierwszaFura = new Car("Volkswagen Polo", 100000, 60000, 80000);
+        pierwszaFura.showCarInfo();
 
 
 
